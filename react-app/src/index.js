3a91e8eb9cb28a4f8ba92567308c6bb239b04b84
Moiz Ahmad <moiznahmad@gmail.com>
2021-09-21 14:15:57 -0700

    added react app project folder

---
+++ react-app/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux';
+import './index.css';
+import App from './App';
+import configureStore from './store';
+
+const store = configureStore();
+
+ReactDOM.render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
