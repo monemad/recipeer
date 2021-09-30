58b227c2dfa0f95f9f3ab577b9ad9dfa141650ef
Moiz Ahmad <moiznahmad@gmail.com>
2021-09-30 13:58:26 -0700

    fixed bug with profile on recipe creation without image

--- react-app/src/components/RecipeComponents/RecipeCard/index.js
+++ react-app/src/components/RecipeComponents/RecipeCard/index.js
@@ -6,7 +6,7 @@ function RecipeCard({ recipe }) {
         <Link to={`/recipes/${recipe.id}`}>
             <div className='recipe-card'>
                 <div className='card-picture-div'>
-                    <img className='card-picture' src={recipe.pictures[0].imgUrl}/>
+                    <img className='card-picture' src={recipe.pictures[0]?.imgUrl}/>
                 </div>
                 <div className='card-details'>
                     <h3>{recipe.title}</h3>
