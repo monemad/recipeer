51a3f1e3c5def417cf5017ed4a24c3d28e0e7702
Moiz Ahmad <moiznahmad@gmail.com>
2022-01-10 10:00:52 -0800

    sorted shopping list by recipe title, then by order

--- react-app/src/components/RecipeComponents/UserIngredients/index.js
+++ react-app/src/components/RecipeComponents/UserIngredients/index.js
@@ -2,10 +2,14 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 function UserIngredients({ user }) {
-    console.log("Shopping List:", user.shoppingList);
     const ingredients = useSelector(state => state.ingredients);
     const units = useSelector(state => state.units);
     const recipes = useSelector(state => state.recipes);
+    let shoppingList = user.shoppingList;
+
+    shoppingList.sort((a, b) => a.recipeId === b.recipeId ? a.order - b.order : recipes[b.recipeId].name < recipes[a.recipeId].name)
+
+    console.log("sorted:", shoppingList)
 
     return (
         <>
@@ -13,7 +17,11 @@ function UserIngredients({ user }) {
                 <h2 className='header'>Shopping List</h2>
                 <div className='recipe-ingredients'>
                     {
-                        
+                        shoppingList.map(ing => 
+                            <div key={ing.id} className='recipe-ingredient'>
+                                {ing.multiplier * ing.quantity} {units[ing.unitId].name} {ingredients[ing.ingredientId].name}
+                            </div>
+                        )
                     }
                 </div>
             </div>
